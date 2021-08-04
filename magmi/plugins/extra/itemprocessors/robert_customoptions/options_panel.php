feb05d5c253a111d81703a08513d2eeef8e3aa12
robert-chefsdeal <robert@chefsdeal.com>
2021-08-04 20:55:06 +0300

    robert_customoptions

---
+++ magmi/plugins/extra/itemprocessors/robert_customoptions/options_panel.php
@@ -0,0 +1,5 @@
+<div class="plugin_description">
+	<p>This is a clone of Custom Options plugin by Pablo & Dweeves which lets you import custom options for products.</p>
+	<p>Put your custom options data in <strong>robert_custom_options</strong> column according to the format below:</p>
+	<p><strong>field_data@value_data;field_data@value_data;...</strong></p>
+</div>
